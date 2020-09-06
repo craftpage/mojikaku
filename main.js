46bf63bf80310826c560d57f474acda9a5f446af
craftpage <craftpage@gmail.com>
2020-09-06 10:59:32 +0900

    2 commit

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ var app = new Vue({
     el: '#app',
     data: {
         character: '文字の入力',
-        size: 20
+        size: 70
     },
     methods: {
         big: function(){
             this.size += 10;
         },
         small: function(){
-            if (this.size > 20) {
+            if (this.size > 70) {
                 this.size -= 10
             }
         }
